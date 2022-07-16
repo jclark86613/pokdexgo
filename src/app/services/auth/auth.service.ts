61484a38fa1a06099ddfdddcc98efa0723967b3b
jclark <79856316+jamie-clark-onscale@users.noreply.github.com>
2022-07-16 17:46:08 +0100

    prepare for angular 11 update

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,28 +1,28 @@
 import { Injectable } from '@angular/core';
-import { FacebookAuthProvider, GoogleAuthProvider } from '@angular/fire/auth';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
+// import { FacebookAuthProvider, GoogleAuthProvider } from '@angular/fire/auth';
+// import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private angularFireAuth: AngularFireAuth) {}
+  // constructor(private angularFireAuth: AngularFireAuth) {}
 
   // login({ email, password }: LoginData): Promise<unknown> {
   //   return this.angularFireAuth.signInWithEmailAndPassword(email, password);
   // }
-  loginWithGoogle(): Promise<unknown> {
-    return this.angularFireAuth.signInWithPopup(new GoogleAuthProvider());
-  }
-  loginWithFacebook(): Promise<unknown> {
-    return this.angularFireAuth.signInWithPopup(new FacebookAuthProvider());
-  }
+  // loginWithGoogle(): Promise<unknown> {
+  //   // return this.angularFireAuth.signInWithPopup(new GoogleAuthProvider());
+  // }
+  // loginWithFacebook(): Promise<unknown> {
+  //   // return this.angularFireAuth.signInWithPopup(new FacebookAuthProvider());
+  // }
 
   // register({ email, password }: LoginData): Promise<unknown> {
   //   return this.angularFireAuth.createUserWithEmailAndPassword(email, password);
   // }
 
-  logout(): Promise<void> {
-    return this.angularFireAuth.signOut();
-  }
+  // logout(): Promise<void> {
+  //   return this.angularFireAuth.signOut();
+  // }
 }
