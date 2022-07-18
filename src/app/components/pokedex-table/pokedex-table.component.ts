46bb5ddfec2e160f415f335198e50b9f9a48e269
jclark <79856316+jamie-clark-onscale@users.noreply.github.com>
2022-07-18 22:16:51 +0100

    sort by column and some styping updates

--- src/app/components/pokedex-table/pokedex-table.component.ts
+++ src/app/components/pokedex-table/pokedex-table.component.ts
@@ -1,9 +1,10 @@
-import { CdkTableDataSourceInput } from '@angular/cdk/table';
+
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { combineLatest } from 'rxjs';
 import { PokemonDataService } from 'src/app/services/pokemon-data/pokemon-data.service';
-import { Pokedex, Pokemon, UserPokedex } from 'src/app/services/pokemon-data/pokemon-data.types';
-
+import { Pokemon, UserPokedex } from 'src/app/services/pokemon-data/pokemon-data.types';
+import { take } from 'rxjs/operators';
 @Component({
   selector: 'app-pokedex-table',
   templateUrl: './pokedex-table.component.html',
@@ -18,6 +19,8 @@ export class PokedexTableComponent implements OnInit {
   private perPage: number = 50;
 
   private updateTimeout: ReturnType<typeof setTimeout>;
+  private orderAsending: boolean = false;
+  private sortedColumn: string = 'id';
 
   private emptyPokemon = {
     normal: false,
@@ -29,34 +32,93 @@ export class PokedexTableComponent implements OnInit {
     purified: false,
   }
   public loading: boolean = true;
-  public displayedColumns: string[] = ['id', 'image', 'name', 'standard', 'shiny', 'lucky', 'perfect', 'threestar', 'shadow', 'purified'];
+  public displayedColumns: string[] = ['id', 'image', 'name', 'normal', 'shiny', 'lucky', 'perfect', 'threestar', 'shadow', 'purified'];
+  public checklist: string[] = ['normal', 'shiny', 'lucky', 'perfect', 'threestar', 'shadow', 'purified'];
 
   constructor(private pokemonDataService: PokemonDataService) {}
 
   ngOnInit(): void {
     const api = this.pokemonDataService;
-    combineLatest([api.pokedex, api.userPokedex]).subscribe(([pokedex, userPokedex]) => {
+    combineLatest([api.pokedex, api.userPokedex]).pipe(take(1)).subscribe(([pokedex, userPokedex]) => {
       // new user, create object
+      const emptyPokedex = JSON.parse(JSON.stringify(Array(Object.values(pokedex).length + 1).fill(this.emptyPokemon)));
       if (!userPokedex) {
-        const emptyPokedex = JSON.parse(JSON.stringify(Array(Object.values(pokedex).length).fill(this.emptyPokemon)));
         this.pokemonDataService.createNewUserPokedex({...emptyPokedex});
         return;
       }
-
       this.pokedex = Object.values(pokedex);
-      this.userPokedex = userPokedex;
+      this.userPokedex = {...emptyPokedex, ...userPokedex};
       this.resetPage();
+      this.sortColumn(this.sortedColumn)
       this.loading = false;
     });
   }
 
-  private resetPage(): void {
-    if (!this.tableData) {
-      this.page = 0;
-      const start = this.page * this.perPage;
-      const end = start + this.perPage;
-      this.tableData = this.pokedex.slice(start,end);
+  public sortColumn(value: string): void {
+    if (this.sortedColumn !== value) {
+      this.sortedColumn = value;
+      this.orderAsending = true;
+    } else {
+      this.orderAsending = !this.orderAsending;
     }
+
+    if (!this.checklist.includes(value)) {
+      this.sortByIdentifier(value);
+    } else {
+      this.sortByChecklist(value);
+    }
+
+    this.resetPage();
+  }
+
+  private sortByIdentifier(value: string): void {
+    this.pokedex.sort((a:Pokemon, b: Pokemon) => {
+      let output = 0;
+      switch(true) {
+        case a[value] > b[value]:
+          output = 1;
+          break;
+        case a[value] < b[value]:
+          output = -1;
+          break;
+        default:
+          output = a.id > b.id ? 1 : -1;
+          break;
+      }
+      return output * (this.orderAsending ? 1 : -1);
+    })
+  }
+
+  public sortByChecklist(value: string): void {
+    this.pokedex.sort((a:Pokemon, b: Pokemon) => {
+      let output = 0;
+      const usera = this.userPokedex[a.id][value] ? 1 : -1;
+      const userb = this.userPokedex[b.id][value] ? 1 : -1;
+      if (!usera || !userb) {
+        return;
+      }
+
+      switch(true) {
+        case usera > userb:
+          output = 1 * (this.orderAsending ? 1 : -1);
+          break;
+        case usera < userb:
+          output = -1 * (this.orderAsending ? 1 : -1);
+          break;
+        default:
+          output = a.id > b.id ? 1 : -1;
+          break;
+      }
+
+      return output;
+    })
+  }
+
+  private resetPage(): void {
+    this.page = 0;
+    const start = this.page * this.perPage;
+    const end = start + this.perPage;
+    this.tableData = this.pokedex.slice(start,end);
   }
 
   public nextPage(): void {
