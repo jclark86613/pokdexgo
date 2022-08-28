82a6555b942ce5723d1f0e3f94cef0276afc7018
Jamie Clark <jamieclark333@hotmail.co.uk>
2022-08-28 15:30:06 +0100

    order forms list to match in game pokedex

--- src/app/components/user-profile-flyout/user-profile-flyout.component.ts
+++ src/app/components/user-profile-flyout/user-profile-flyout.component.ts
@@ -3,7 +3,7 @@ import { User } from 'firebase/auth';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { PokedexDataService } from 'src/app/services/pokedex-data/pokedex-data.service';
-import { EMPTY_POKEDEX_COUNT, PokedexCounts } from 'src/app/services/pokedex-data/pokedex-data.types';
+import { EMPTY_POKEDEX_COUNT, PokedexCounts, STANDARD_POKEMON_FORMS_EMUN } from 'src/app/services/pokedex-data/pokedex-data.types';
 
 @Component({
   selector: 'app-user-profile-flyout',
@@ -15,6 +15,15 @@ export class UserProfileFlyoutComponent {
   public icon: string = ''; 
   public PokedexCounts$: Observable<PokedexCounts> = this.pokedexDataService.pokedexCounts;
   public userCounts = JSON.parse(JSON.stringify(EMPTY_POKEDEX_COUNT));
+  public readonly pokedexFormOrder = [
+    STANDARD_POKEMON_FORMS_EMUN.PURIFIED,
+    STANDARD_POKEMON_FORMS_EMUN.SHADOW,
+    STANDARD_POKEMON_FORMS_EMUN.PERFECT,
+    STANDARD_POKEMON_FORMS_EMUN.THREESTAR,
+    STANDARD_POKEMON_FORMS_EMUN.SHINY,
+    STANDARD_POKEMON_FORMS_EMUN.LUCKY,
+    STANDARD_POKEMON_FORMS_EMUN.NORMAL
+  ];
 
   constructor(private pokedexDataService: PokedexDataService, public authService: AuthService) {
     this.authService.user.subscribe((user:User) => {
